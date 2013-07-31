e652f8c6f2c5a9743b4affdf6af95a3ded6c34c8
Eddie Kim <eddotman@gmail.com>
2013-07-30 21:22:29 -0400

    added README

---
+++ README.md
@@ -0,0 +1,16 @@
+HXMA FILE PARSER
+================
+
+Overview
+--------
+
+Hxma File Parser converts data files from the HXMA beamline at the Canadian Light Source into a cleaner format.
+
+Usage
+-----
+
+First, download the files: [hxma-file-parser.py](). Next, make sure that you have Python and NumPy installed. In Ubuntu, you can do this by typing:  
+
+`sudo apt-get install python python-numpy`  
+
+Then, edit the `hxma-file-parser.py` file and insert the filenames where appropriate. Make sure to have the target file in the same directory as the python file.
--- hxma-file-parser.py
+++ hxma-file-parser.py
@@ -3,26 +3,35 @@ from numpy import *
 class Parser:
 
 	def __init__(self, f = None):
-		self.f_raw = f
+		try:
+			self.f_raw = f
+		except:
+			print "Failed to load file: check filename."
 		self.f_fmt = None
 		print "HXMA file parser created."
 
 	def parse_file(self):
 		if not self.f_raw == None:
-			self.f_fmt = loadtxt(self.f_raw, delimiter=",")
-			print "File loaded."
+			try:
+				self.f_fmt = loadtxt(self.f_raw, delimiter=",")
+				print "File loaded."
+			except:
+				print "Failed to parse file: check formatting."
 		else:
 			print "No file loaded."
 
 	def save_file(self, path):
 		if not self.f_fmt == None:
-			savetxt(path, self.f_fmt)
-			print "File parsed."
+			try:
+				savetxt(path, self.f_fmt)
+				print "File parsed."
+			except:
+				print "Failed to save file: check write permissions."
 		else:
 			print "No file parsed."
 
 if __name__ == "__main__":
-	p1 = Parser(f="INPUT_FILE_NAME_HERE")
+	p1 = Parser(f = "INPUT_FILE_NAME_HERE")
 	p1.parse_file()
 	p1.save_file("OUTPUT_FILE_NAME_HERE")
 
